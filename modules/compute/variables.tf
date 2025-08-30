22f3ee835428e564b35f67adce9cf33c20b1ca37
Shubham85318 <144038002+Shubham85318@users.noreply.github.com>
2025-08-30 19:48:22 +0530

    - VPC module: variables, VPC, subnets, route tables, outputs - Compute module: security group, Amazon Linux 2 EC2 instance, outputs

---
+++ modules/compute/variables.tf
@@ -0,0 +1,16 @@
+﻿variable "vpc_id" {
+  type = string
+}
+
+variable "public_subnet_ids" {
+  type = list(string)
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,16 @@
+﻿variable "vpc_cidr" {
+  type = string
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "public_subnet_cidrs" {
+  type = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  type = list(string)
+}
+
